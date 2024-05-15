173a54f21137acbdc638ce41b70bd4c60b94a1e5
theab10hd <theab10hd@gmail.com>
2024-05-15 18:36:47 +0530

    Layout Updated

--- script/script.js
+++ script/script.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     window.setTimeout(
         document.querySelector("svg").classList.add("animated"),
-        1000
+        500
     );
 });
+
+
+function toggleSemester(sem) {
+    var semElements = document.querySelectorAll('.sem');
+
+    semElements.forEach(function (element) {
+        element.classList.add("d-none");
+    });
+    document.querySelectorAll('.home-bottons-non').forEach(function (btn) {
+        btn.classList.remove("selectedBtn");
+    });
+
+    var semElementToShow = document.getElementById("sem" + sem);
+    if (semElementToShow) {
+        semElementToShow.classList.remove("d-none");
+        document.getElementById('btnSem' + sem).classList.add('selectedBtn');
+    }
+}
